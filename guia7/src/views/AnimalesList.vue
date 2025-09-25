47d366d1bc000295c9efb1adc464cb57a54d1a8e
elqnubiz57astro <elqnubiz57astro@gmail.com>
2025-09-25 09:57:06 -0600

    si2

---
+++ guia7/src/views/AnimalesList.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>Lista de animales</h1>
+        <div id="card">
+        <h3>Nuevo animal</h3>
+    <input type="text" placeholder="Escribe un animal">
+<br>
+    <button>Agregar</button>
+</div>
+    <br>
+<div id="card">
+<h4>Animales</h4>
+</div>
+</template>
+<script>
+export default {
+name: 'AnimalesList'
+}
+</script>
+<style scoped>
+#card {
+border: 1px solid yellowgreen;
+border-radius: 40px;
+margin: auto;
+width: 50%;
+padding: 20px;
+}
+button{
+border: 1px solid darkolivegreen;
+width: 180px;
+padding: 10px;
+margin: 20px;
+background-color: yellowgreen;
+border-radius: 40px;
+color: black;
+}
+</style>
---
+++ guia7/src/views/CalculadoraView.vue
@@ -0,0 +1,60 @@
+<template>
+<h1>Calculadora</h1>
+<div id="card">
+<table>
+<tr>
+<td>Operación</td>
+<td>
+<select>
+<option></option>
+</select>
+</td>
+</tr>
+<tr>
+<td>Número 1</td>
+<td><input type="number"></td>
+</tr>
+<tr>
+<td>Número 2</td>
+<td><input type="number"></td>
+</tr>
+<tr>
+<td colspan="2">
+<hr>
+<button>Calcular</button>
+</td>
+</tr>
+</table>
+</div>
+<br>
+<div id="card">
+<h4>Respuesta: </h4>
+</div>
+</template>
+<script>
+export default {
+name: 'CalculadoraView'
+}
+</script>
+<style scoped>
+#card {
+border: 1px solid yellowgreen;
+border-radius: 40px;
+margin: auto;
+width: 50%;
+padding: 20px;
+}
+button{
+border: 1px solid darkolivegreen;
+width: 100%;
+padding: 10px;
+background-color: yellowgreen;
+color: black;
+}
+table {
+margin: auto;
+}
+select {
+width: 100%;
+}
+</style>
